d0e2929533ad2dd55c6a62ff3e27c821bb64c6a2
Alsa <65710458+Alsa2@users.noreply.github.com>
2022-10-04 09:51:46 +0900

    cryptography now works

--- TEMP/TEMP4.py
+++ TEMP/TEMP4.py
@@ -1,16 +1,148 @@
-# import required module
-from cryptography.fernet import Fernet
+def encrypt_file(stdsrc):
+    """
+    Encrypts a file using the key
+    """
+    h, w = stdsrc.getmaxyx()
+    stdsrc.clear()
+    textmessage = ("Enter Encryption Key: ")
 
-fernet = Fernet("2wFYfwlLpg4iy_k-wkGxNUH3pGLYdzjFbEyf4jQJfiY=")
- 
-# opening the original file to encrypt
-with open('transactions.csv', 'rb') as file:
-    original = file.read()
-     
-# encrypting the file
-encrypted = fernet.encrypt(original)
- 
-# opening the file in write mode and
-# writing the encrypted data
-with open('transactions.csv', 'wb') as encrypted_file:
-    encrypted_file.write(encrypted)
+    # ask for encryption key
+    win = curses.newwin(1, 40, h//2, w//2 - 20)
+    box = curses.textpad.Textbox(win)
+    curses.textpad.rectangle(stdsrc, h//2-2, w//2-(len(textmessage)//2+10), h//2+2, w//2+len(textmessage)//2+10)
+    stdsrc.addstr(h//2 - 1, w//2-len(textmessage)//2, textmessage)
+    stdsrc.refresh()
+    box.edit()
+    key = box.gather().replace("/n", "")
+    key = key.replace(" ", "")
+    # remove the window
+    win.clear()
+    stdsrc.clear()
+    time.sleep(1)
+
+    defaultkey = False
+    if str(key) == "default":
+        with open('filekey.key', 'r') as filekey:
+            key = filekey.read()
+        defaultkey = True
+        stdsrc.refresh()
+        time.sleep(2)
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Defaut Key")
+            stdsrc.addstr(h//2+2, w//2, key)
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+    else:
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Key")
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+
+        defaultkey = False
+    if str(key) == "default":
+        with open('filekey.key', 'rb') as filekey:
+            key = filekey.read()
+        defaultkey = True
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Defaut Key")
+            stdsrc.addstr(h//2+2, w//2, key)
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+    else:
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Key")
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+        
+    
+    stdsrc.addstr(h//2+4, w//2 - 10, "Loading Key...")
+    h, w = stdsrc.getmaxyx()
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(0, 30):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(30/2), 30))
+    if defaultkey:
+        stdsrc.addstr(h//2+4, w//2 - 10, "Default Key Loaded  ")
+    else:
+        stdsrc.addstr(h//2+4, w//2-10, "Key loaded   ")
+    stdsrc.refresh()
+
+    # loading file
+    stdsrc.addstr(h//2+5, w//2 - 10, "Loading File...")
+    with open('transactions.csv', 'rb') as enc_file:
+        decrypted = enc_file.read()
+    
+    h, w = stdsrc.getmaxyx()
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(30, 50):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(50/2), 50))
+    stdsrc.addstr(h//2+5, w//2-10, "Loaded Decrypted File   ")
+    stdsrc.refresh()
+
+    stdsrc.addstr(h//2+6, w//2 - 10, "Encrypting File...")
+    try:
+        encrypted = fernet.encrypt(decrypted)
+    except Exception as e:
+        stdsrc.clear()
+        stdsrc.addstr(h//2, w//2, "Invalid Key")
+        stdsrc.refresh()
+        stdsrc.addstr(h//2 + 2, w//2, (e))
+        stdsrc.refresh()
+        time.sleep(4)
+        exit(e)
+    
+    
+    
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(50, 75):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(75/2), 75))
+    stdsrc.addstr(h//2+6, w//2-10, "File Encrypted    ")
+    stdsrc.refresh()
+
+    stdsrc.addstr(h//2+7, w//2 - 10, "Remplacing File...")
+    
+    with open('transactions.csv', 'wb') as dec_file:
+        dec_file.write(encrypted)
+    
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(75, 101):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(101/2), 100))
+    stdsrc.addstr(h//2+7, w//2-10, "File Replaced     ")
+    stdsrc.refresh()
+
+    time.sleep(1)
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(101/2), 100))
+    stdsrc.addstr(h//2+8, w//2-10, "Encryption Succesfully Complete")
+    stdsrc.refresh()
+    time.sleep(1)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import e
 import curses
 import curses.textpad
 from email.message import Message
@@ -232,10 +233,9 @@ def decrypt_file(stdsrc):
     stdsrc.clear()
     time.sleep(1)
 
-
     defaultkey = False
     if str(key) == "default":
-        with open('filekey.key', 'rb') as filekey:
+        with open('filekey.key', 'r') as filekey:
             key = filekey.read()
         defaultkey = True
         try:
@@ -243,6 +243,7 @@ def decrypt_file(stdsrc):
         except:
             stdsrc.clear()
             stdsrc.addstr(h//2, w//2, "Invalid Defaut Key")
+            stdsrc.addstr(h//2+2, w//2, key)
             stdsrc.refresh()
             time.sleep(2)
             exit()
@@ -255,8 +256,33 @@ def decrypt_file(stdsrc):
             stdsrc.refresh()
             time.sleep(2)
             exit()
+
+        defaultkey = False
+    if str(key) == "default":
+        with open('filekey.key', 'rb') as filekey:
+            key = filekey.read()
+        defaultkey = True
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Defaut Key")
+            stdsrc.addstr(h//2+2, w//2, key)
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+    else:
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Key")
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+        
     
-    stdsrc.addstr(h//2+4, w//2 - 35, "Loading Key...")
+    stdsrc.addstr(h//2+4, w//2 - 10, "Loading Key...")
     h, w = stdsrc.getmaxyx()
     win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
     for i in range(0, 30):
@@ -266,13 +292,13 @@ def decrypt_file(stdsrc):
     win2.clear()
     stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(30/2), 30))
     if defaultkey:
-        stdsrc.addstr(h//2+4, w//2 - 35, "Default Key Loaded  ")
+        stdsrc.addstr(h//2+4, w//2 - 10, "Default Key Loaded  ")
     else:
         stdsrc.addstr(h//2+4, w//2-10, "Key loaded   ")
     stdsrc.refresh()
 
     # loading file
-    stdsrc.addstr(h//2+4, w//2 - 35, "Loading File...")
+    stdsrc.addstr(h//2+5, w//2 - 10, "Loading File...")
     with open('transactions.csv', 'rb') as enc_file:
         encrypted = enc_file.read()
     """
@@ -293,19 +319,19 @@ def decrypt_file(stdsrc):
     stdsrc.addstr(h//2+5, w//2-10, "Loaded Encrypted File   ")
     stdsrc.refresh()
 
-    stdsrc.addstr(h//2+4, w//2 - 35, "Decrypting File...")
-    """
+    stdsrc.addstr(h//2+6, w//2 - 10, "Decrypting File...")
     try:
         decrypted = fernet.decrypt(encrypted)
-    except:
+    except Exception as e:
         stdsrc.clear()
         stdsrc.addstr(h//2, w//2, "Invalid Key")
         stdsrc.refresh()
-        time.sleep(2)
-        exit()
-        """
+        stdsrc.addstr(h//2 + 2, w//2, (e))
+        stdsrc.refresh()
+        time.sleep(4)
+        exit(e)
     
-    decrypted = fernet.decrypt(encrypted)
+    
     
     win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
     for i in range(50, 75):
@@ -314,11 +340,14 @@ def decrypt_file(stdsrc):
         win2.refresh()
     win2.clear()
     stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(75/2), 75))
-    stdsrc.addstr(h//2+5, w//2-10, "File Decrypted   ")
+    stdsrc.addstr(h//2+6, w//2-10, "File Decrypted    ")
     stdsrc.refresh()
 
-    stdsrc.addstr(h//2+4, w//2 - 35, "Remplacing File...")
-    decrypted = fernet.decrypt(encrypted)
+    stdsrc.addstr(h//2+7, w//2 - 10, "Remplacing File...")
+    
+    with open('transactions.csv', 'wb') as dec_file:
+        dec_file.write(decrypted)
+    
     win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
     for i in range(75, 101):
         time.sleep(0.01)
@@ -326,14 +355,15 @@ def decrypt_file(stdsrc):
         win2.refresh()
     win2.clear()
     stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(101/2), 100))
-    stdsrc.addstr(h//2+5, w//2-10, "File Replaced   ")
+    stdsrc.addstr(h//2+7, w//2-10, "File Replaced     ")
     stdsrc.refresh()
 
     time.sleep(1)
     stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(101/2), 100))
-    stdsrc.addstr(h//2+5, w//2-10, "Decryption Succesfully Complete")
+    stdsrc.addstr(h//2+8, w//2-10, "Decryption Succesfully Complete")
     stdsrc.refresh()
     time.sleep(1)
+
     
 
 def main_menu(stdsrc, selected_row_idx):
@@ -365,10 +395,159 @@ def main_menu(stdsrc, selected_row_idx):
 
 def screen_exit(stdsrc):
     stdsrc.clear()
+    # ask if the user whats to encrypt
+    stdsrc.addstr(h//2, w//2 - 10, "Do you want to encrypt?")
+    key = stdsrc.getkey()
+    while key 
     exit()
 
 def encrypt_file(stdsrc):
-    pass
+    """
+    Encrypts a file using the key
+    """
+    h, w = stdsrc.getmaxyx()
+    stdsrc.clear()
+    textmessage = ("Enter Encryption Key: ")
+
+    # ask for encryption key
+    win = curses.newwin(1, 40, h//2, w//2 - 20)
+    box = curses.textpad.Textbox(win)
+    curses.textpad.rectangle(stdsrc, h//2-2, w//2-(len(textmessage)//2+10), h//2+2, w//2+len(textmessage)//2+10)
+    stdsrc.addstr(h//2 - 1, w//2-len(textmessage)//2, textmessage)
+    stdsrc.refresh()
+    box.edit()
+    key = box.gather().replace("/n", "")
+    key = key.replace(" ", "")
+    # remove the window
+    win.clear()
+    stdsrc.clear()
+    time.sleep(1)
+
+    defaultkey = False
+    if str(key) == "default":
+        with open('filekey.key', 'r') as filekey:
+            key = filekey.read()
+        defaultkey = True
+        stdsrc.refresh()
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Defaut Key")
+            stdsrc.addstr(h//2+2, w//2, key)
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+    else:
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Key")
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+
+        defaultkey = False
+    if str(key) == "default":
+        with open('filekey.key', 'rb') as filekey:
+            key = filekey.read()
+        defaultkey = True
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Defaut Key")
+            stdsrc.addstr(h//2+2, w//2, key)
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+    else:
+        try:
+            fernet = Fernet(key)
+        except:
+            stdsrc.clear()
+            stdsrc.addstr(h//2, w//2, "Invalid Key")
+            stdsrc.refresh()
+            time.sleep(2)
+            exit()
+        
+    
+    stdsrc.addstr(h//2+4, w//2 - 10, "Loading Key...")
+    h, w = stdsrc.getmaxyx()
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(0, 30):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(30/2), 30))
+    if defaultkey:
+        stdsrc.addstr(h//2+4, w//2 - 10, "Default Key Loaded  ")
+    else:
+        stdsrc.addstr(h//2+4, w//2-10, "Key loaded   ")
+    stdsrc.refresh()
+
+    # loading file
+    stdsrc.addstr(h//2+5, w//2 - 10, "Loading File...")
+    with open('transactions.csv', 'rb') as enc_file:
+        decrypted = enc_file.read()
+    
+    h, w = stdsrc.getmaxyx()
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(30, 50):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(50/2), 50))
+    stdsrc.addstr(h//2+5, w//2-10, "Loaded Decrypted File   ")
+    stdsrc.refresh()
+
+    stdsrc.addstr(h//2+6, w//2 - 10, "Encrypting File...")
+    try:
+        encrypted = fernet.encrypt(decrypted)
+    except Exception as e:
+        stdsrc.clear()
+        stdsrc.addstr(h//2, w//2, "Invalid Key")
+        stdsrc.refresh()
+        stdsrc.addstr(h//2 + 2, w//2, (e))
+        stdsrc.refresh()
+        time.sleep(4)
+        exit(e)
+    
+    
+    
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(50, 75):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(75/2), 75))
+    stdsrc.addstr(h//2+6, w//2-10, "File Encrypted    ")
+    stdsrc.refresh()
+
+    stdsrc.addstr(h//2+7, w//2 - 10, "Remplacing File...")
+    
+    with open('transactions.csv', 'wb') as dec_file:
+        dec_file.write(encrypted)
+    
+    win2 = curses.newwin(1, 80, h//2+3, w//2 - 35)
+    for i in range(75, 101):
+        time.sleep(0.01)
+        win2.addstr(0, 0, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(i/2), i))
+        win2.refresh()
+    win2.clear()
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(101/2), 100))
+    stdsrc.addstr(h//2+7, w//2-10, "File Replaced     ")
+    stdsrc.refresh()
+
+    time.sleep(1)
+    stdsrc.addstr(h//2+3, w//2 - 35, "Progress: [{0:50s}] {1:.1f}%".format('#' * int(101/2), 100))
+    stdsrc.addstr(h//2+8, w//2-10, "Encryption Succesfully Complete")
+    stdsrc.refresh()
+    time.sleep(1)
 
 #curses.wrapper(decrypt_file)
 curses.wrapper(main)
